d123cc4db424c4e1fb3a5644aa57506b1032387e
Balalomba Barandao <barand14@gmail.com>
2024-05-09 18:24:56 -0400

    tested shapes

--- index.js
+++ index.js
@@ -54,6 +54,16 @@ function init(){
             newShape = new Triangle(response.shapecolor)
         }
 
+        const newSvgTag = 
+        `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+        ${newShape.render()}
+
+        <text x="150" y="150" font-size="60" text-anchor="middle" fill="${response.textColor}">${response.text}</text>
+      
+        </svg>
+        `
+        fs.writeFileSync("newLogo.svg", newSvgTag)
     })
 }
 
